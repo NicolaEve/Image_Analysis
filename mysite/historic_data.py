7f4af05745150667a5d812fc1471eec4037c9184
NicolaEve <nicevecomp@gmail.com>
2021-06-15 16:37:45 +0100

    included a cutoff to historic data

--- mysite/historic_data.py
+++ mysite/historic_data.py
@@ -40,68 +40,72 @@ def historic(energy, filepath):
         created = os.stat(xim_file).st_ctime
         date = datetime.fromtimestamp(created)
 
-        # enter date and beam energy to database
-        # return the mpc event id
-        statement = """declare @mpc_event_id int;
-                                   EXEC @mpc_event_id = Insert_MPC_Event ?,?;
-                                   SELECT @mpc_event_id as mpc_event_id; """
-        cursor.execute(statement, [energy, date])
-        mpc_event_id = cursor.fetchval()
-        cursor.commit()
-
-        # extract the data from the xim file
-        # convert xim to png, first copy the file into the media directory
-        new_name = os.path.join(media_directory, "Beam.xim")
-        # saving as the same name every time will overwrite the previous one
-        shutil.copy(xim_file, new_name)
-        # now run ximmer
-        ximmer = os.path.join(media_directory, "ximmerAll.py")
-        cmd = str("python " + ximmer + " -d " + media_directory)
-        os.system(cmd)
-        # get the filename of the image file
-        for new_file in os.listdir(xim_directory):
-            if new_file.endswith(".png") or new_file.endswith(".jpeg"):
-                image_file = os.path.join(xim_directory, new_file)
-
-                # get the calibrated symmetry, flatness and beam centre shift
-                obj = main.NewImages(energy, image_file)
-                crossline_symm, inline_symm = obj.symmetry(transformed=True)
-                crossline_flat, inline_flat = obj.flatness(transformed=True)
-                centre_shift = obj.centre_shift()
-                # enter into database
-                # Binary: inline is 1, transformed is 1
-                cursor.execute("EXEC Insert_Symm_Flat_Data ?,?,?,?,?",
-                               [mpc_event_id, 1, 1, inline_symm, inline_flat])
-                cursor.commit()
-                # repeat for crossline
-                cursor.execute("EXEC Insert_Symm_Flat_Data ?,?,?,?,?",
-                               [mpc_event_id, 0, 1, crossline_symm, crossline_flat])
-                cursor.commit()
-
-                # repeat for untransformed data, before calibration applied
-                crossline_symm, inline_symm = obj.symmetry(transformed=False)
-                crossline_flat, inline_flat = obj.flatness(transformed=False)
-                cursor.execute("EXEC Insert_Symm_Flat_Data ?,?,?,?,?",
-                               [mpc_event_id, 1, 0, inline_symm, inline_flat])
-                cursor.commit()
-                cursor.execute("EXEC Insert_Symm_Flat_Data ?,?,?,?,?",
-                               [mpc_event_id, 0, 0, crossline_symm, crossline_flat])
-                cursor.commit()
-
-        # extract the MPC self-reported results in the excel file
-        f = open(results_file)
-        csv_reader_object = csv.reader(f)
-
-        # structure of these is [name, value, threshold, pass/fail, date]
-        for line in csv_reader_object:
-            name = str(line[0])
-            if name.find("BeamOutputChange") != -1:
-                cursor.execute("EXEC Insert_MPC_Output_Change_Data ?,?", [mpc_event_id, line[1]])
-            if name.find("BeamUniformityChange") != -1:
-                cursor.execute("EXEC Insert_Uniformity_Change_Data ?,?", [mpc_event_id, line[1]])
-            if name.find("BeamCenterShift") != -1:
-                cursor.execute("EXEC Insert_Beam_Center_Shift_Data ?,?,?", [mpc_event_id, line[1], centre_shift])
+        # get the most recent ones
+        # chnage this to get the most recent file that was in the database?
+        cutoff = date > datetime(2021, 3, 1, 12, 17, 9, 29375)
+        if cutoff is True:
+            statement = """declare @mpc_event_id int;
+                                       EXEC @mpc_event_id = Insert_MPC_Event ?,?;
+                                       SELECT @mpc_event_id as mpc_event_id; """
+            cursor.execute(statement, [energy, date])
+            mpc_event_id = cursor.fetchval()
             cursor.commit()
+            # enter date and beam energy to database
+            # return the mpc event id
+
+            # extract the data from the xim file
+            # convert xim to png, first copy the file into the media directory
+            new_name = os.path.join(media_directory, "Beam.xim")
+            # saving as the same name every time will overwrite the previous one
+            shutil.copy(xim_file, new_name)
+            # now run ximmer
+            ximmer = os.path.join(media_directory, "ximmerAll.py")
+            cmd = str("python " + ximmer + " -d " + media_directory)
+            os.system(cmd)
+            # get the filename of the image file
+            for new_file in os.listdir(xim_directory):
+                if new_file.endswith(".png") or new_file.endswith(".jpeg"):
+                    image_file = os.path.join(xim_directory, new_file)
+
+                    # get the calibrated symmetry, flatness and beam centre shift
+                    obj = main.NewImages(energy, image_file)
+                    crossline_symm, inline_symm = obj.symmetry(transformed=True)
+                    crossline_flat, inline_flat = obj.flatness(transformed=True)
+                    centre_shift = obj.centre_shift()
+                    # enter into database
+                    # Binary: inline is 1, transformed is 1
+                    cursor.execute("EXEC Insert_Symm_Flat_Data ?,?,?,?,?",
+                                   [mpc_event_id, 1, 1, inline_symm, inline_flat])
+                    cursor.commit()
+                    # repeat for crossline
+                    cursor.execute("EXEC Insert_Symm_Flat_Data ?,?,?,?,?",
+                                   [mpc_event_id, 0, 1, crossline_symm, crossline_flat])
+                    cursor.commit()
+
+                    # repeat for untransformed data, before calibration applied
+                    crossline_symm, inline_symm = obj.symmetry(transformed=False)
+                    crossline_flat, inline_flat = obj.flatness(transformed=False)
+                    cursor.execute("EXEC Insert_Symm_Flat_Data ?,?,?,?,?",
+                                   [mpc_event_id, 1, 0, inline_symm, inline_flat])
+                    cursor.commit()
+                    cursor.execute("EXEC Insert_Symm_Flat_Data ?,?,?,?,?",
+                                   [mpc_event_id, 0, 0, crossline_symm, crossline_flat])
+                    cursor.commit()
+
+            # extract the MPC self-reported results in the excel file
+            f = open(results_file)
+            csv_reader_object = csv.reader(f)
+
+            # structure of these is [name, value, threshold, pass/fail, date]
+            for line in csv_reader_object:
+                name = str(line[0])
+                if name.find("BeamOutputChange") != -1:
+                    cursor.execute("EXEC Insert_MPC_Output_Change_Data ?,?", [mpc_event_id, line[1]])
+                if name.find("BeamUniformityChange") != -1:
+                    cursor.execute("EXEC Insert_Uniformity_Change_Data ?,?", [mpc_event_id, line[1]])
+                if name.find("BeamCenterShift") != -1:
+                    cursor.execute("EXEC Insert_Beam_Center_Shift_Data ?,?,?", [mpc_event_id, line[1], centre_shift])
+                cursor.commit()
     except FileNotFoundError:
         pass
 
